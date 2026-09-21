d90de66576631ca8d95c8634b149f220d437c2f6
ChiragChelani <chirag.chelani17@gmail.com>
2026-09-21 16:03:48 +0530

    reverted backend

--- src/models/Organisations.js
+++ src/models/Organisations.js
@@ -72,15 +72,6 @@ export default function(sequelize) {
       type: DataTypes.BOOLEAN,
       allowNull: true,
       defaultValue: true
-    },
-    is_approved: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0
-    },
-    block_reason: {
-      type: DataTypes.TEXT,
-      allowNull: true
     }
   }, {
     tableName: 'organisations',
